da59e34a475f7348a94613eb755bbcf65c6f6a8e
jaemin <once29@naver.com>
2017-08-16 20:41:06 +0900

    Cafe Moa Store CLient

---
+++ app/src/main/java/mkworld29/mobile/com/cafemoa_store/MainActivity.java
@@ -0,0 +1,13 @@
+package mkworld29.mobile.com.cafemoa_store;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
