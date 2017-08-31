799c205fc03bb643313b4a74bdb1a4ca37e1c8e2
jaemin <once29@naver.com>
2017-08-31 18:38:25 +0900

    [+] SwipeRefreshLayout

--- app/src/main/java/mkworld29/mobile/com/cafemoa_store/OrderActivity.java
+++ app/src/main/java/mkworld29/mobile/com/cafemoa_store/OrderActivity.java
@@ -1,5 +1,6 @@
 package mkworld29.mobile.com.cafemoa_store;
 
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
@@ -7,7 +8,7 @@ import android.widget.ListView;
 
 import mkworld29.mobile.com.cafemoa_store.adapter.OrderListAdapter;
 
-public class OrderActivity extends AppCompatActivity {
+public class OrderActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener{
 
     private ListView lv_order_list;
 
@@ -15,6 +16,10 @@ public class OrderActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
+
+        SwipeRefreshLayout mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_layout);
+        mSwipeRefreshLayout.setOnRefreshListener(this);
+
 
         lv_order_list = (ListView) findViewById(R.id.lv_order_list);
 
@@ -25,5 +30,10 @@ public class OrderActivity extends AppCompatActivity {
         adapter.addItem("아메리카노",2,4,true,false );
 
         lv_order_list.setAdapter(adapter);
+    }
+
+    @Override
+    public void onRefresh() {
+        // 새로고침 소스
     }
 }
